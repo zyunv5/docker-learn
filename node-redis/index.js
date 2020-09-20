505054e3bdca239944f633aa9ac962f416ad523a
zyunv5 <515224526@qq.com>
2020-09-20 23:11:55 +0800

    running

--- node-redis/index.js
+++ node-redis/index.js
@@ -1,33 +1,41 @@
-const Koa = require("koa");
-const Router = require("koa-router");
-const Redis = require("ioredis");
+const Koa = require('koa')
+const Router = require('koa-router')
+const Redis = require('ioredis')
 
-const app = new Koa();
-const router = new Router();
+const app = new Koa()
+const router = new Router()
 const redis = new Redis({
+  host: 'redis',
+  // host: '127.0.0.1',
   port: 6379,
-  host: "db",
-});
+})
 
-router.get("/", (ctx, next) => {
-  ctx.body = "hello world.";
-});
+router.get('/', (ctx, next) => {
+  ctx.body = 'hello world nodejs.'
+})
 
-router.get("/api/json/get", async (ctx, next) => {
-  const result = await redis.get("age");
-  ctx.body = result;
-});
+router.get('/api/json/get', async (ctx, next) => {
+  const result = await redis.get('age')
+  ctx.body = result
+})
 
-router.get("/api/json/set", async (ctx, next) => {
-  const result = await redis.set("age", ctx.query.age);
+router.get('/api/json/set', async (ctx, next) => {
+  const result = await redis.set('age', ctx.query.age)
   ctx.body = {
     status: result,
     age: ctx.query.age,
-  };
-});
+  }
+})
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(3000, () => {
-  console.log("server start at localhost:3000");
-});
+app.listen(
+  {
+    hostname: '0.0.0.0',
+    port: process.env.WEB_PORT || 7001,
+    // not write localhost/127.0.0.1
+  },
+  () => {
+    console.log(`server start`)
+  }
+)
